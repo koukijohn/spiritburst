eba7abed3264e5e6871b3c49e38a2c005d7d1022
koukijohn <koukijohn@hotmail.com>
2018-10-19 09:20:29 -0700

    added binarysearch

---
+++ binarysearch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    This will start a binary search function
+"""
+
+def binarysearch(arr, l, r, x):
+    """
+    """
+    while (l <= r):
+        mid = l + (r - l)/2;
+
+        if (arr[mid] == x):
+            return mid
+        elif (arr[mid] < x):
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+
+result = binarysearh(arr, 0, len(arr) - 1, x)
+
+if result != -1:
+    print("Element at index %d" % result)
+else:
+    print("Element not at index")
